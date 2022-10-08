7f3573197943ccd153126e1c9f120efcf7c889cb
jkniss <jessejames2605@gmail.com>
2022-10-08 13:27:46 -0600

    Created connection to backend API, viewed through Angular SPA

--- app_admin/src/app/trip-listing/trip-listing.component.ts
+++ app_admin/src/app/trip-listing/trip-listing.component.ts
@@ -1,19 +1,38 @@
 import { Component, OnInit } from '@angular/core';
-import { trips } from '../data/trips';
+//import { trips } from '../data/trips';
+import { TripDataService } from '../services/trip-data.service';
+import { Trip } from '../models/trip';
+
 
 @Component({
   selector: 'app-trip-listing',
   templateUrl: './trip-listing.component.html',
-  styleUrls: ['./trip-listing.component.css']
+  styleUrls: ['./trip-listing.component.css'],
+  providers: [TripDataService]
 })
 export class TripListingComponent implements OnInit {
 
   // variable = trips, which is an array of any, and it's assigned the value fo trips from ../data/trips
-  trips: Array<any> = trips;
+  //trips: Array<any> = trips;
+  trips: Trip[];
 
-  constructor() { }
+  message: string;
 
-  ngOnInit() {
+  constructor(private tripDataService: TripDataService) { }
+
+  private getTrips(): void {
+    console.log('Inside TripListingComponent#getTrips');
+    this.message = 'Searching for trips';
+    this.tripDataService
+      .getTrips()
+        .then(foundTrips => {
+          this.message = foundTrips.length > 0 ? '' : 'No trips found';
+          this.trips = foundTrips;
+        });
+  }
+
+  ngOnInit(): void {
+    this.getTrips();
   }
 
 }
